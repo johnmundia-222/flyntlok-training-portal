16c52d54c8420922638d105ef4a1aec6c43031ab
johnmundia-222 <john.mundia@flyntlok.com>
2026-02-19 10:48:03 -0500

    Update firebase.js to use environment variables for Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeyejB8a0ed5I0nPyngYf0A0JxqamH-_c",
-  authDomain: "flyntlok-training-portal.firebaseapp.com",
-  projectId: "flyntlok-training-portal",
-  storageBucket: "flyntlok-training-portal.firebasestorage.app",
-  messagingSenderId: "760353969509",
-  appId: "1:760353969509:web:9c0056dc591b84349abd8c",
-  measurementId: "G-EHWZL20WC9"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
